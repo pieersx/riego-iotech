7b3fa7b83f08e72ac52af43ffbdcc6f69198e2c8
Pieers <Piers_84@hotmail.com>
2024-11-17 20:25:17 -0500

    Update #1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBdQDh-VkRQGcT_7T5ac9XvFVoeT0KH3UY",
-  authDomain: "riego-iotech.firebaseapp.com",
-  databaseURL: "https://riego-iotech-default-rtdb.firebaseio.com",
-  projectId: "riego-iotech",
-  storageBucket: "riego-iotech.firebasestorage.app",
-  messagingSenderId: "163534901539",
-  appId: "1:163534901539:web:ce4eb6d548a1d2d5c47c96",
-  measurementId: "G-SB5FTGJLY8"
-};
+// const firebaseConfig
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://riego-iotech-default-rtdb.firebaseio.com/';
-const supabaseKey = 'AIzaSyBdQDh-VkRQGcT_7T5ac9XvFVoeT0KH3UY';
+const supabaseUrl = '';     // databaseURL
+const supabaseKey = '';     // apiKey
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

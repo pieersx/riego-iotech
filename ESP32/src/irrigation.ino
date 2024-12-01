113c46092d544e0981bb167c8a810213c36b6287
Pieers <Piers_84@hotmail.com>
2024-12-01 18:51:04 -0500

    modified db

--- ESP32/src/irrigation.ino
+++ ESP32/src/irrigation.ino
@@ -28,8 +28,8 @@ const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
 // Firebase credentials
-#define API_KEY "AIzaSyBdQDh-VkRQGcT_7T5ac9XvFVoeT0KH3UY"
-#define DATABASE_URL "https://riego-iotech-default-rtdb.firebaseio.com"
+#define API_KEY ""
+#define DATABASE_URL ""
 
 // Objetos necesarios para Firebase
 FirebaseData fbdo;
